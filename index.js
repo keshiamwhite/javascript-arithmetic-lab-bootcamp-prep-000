65fbc9f29e97a1562aded4420e1103ae85c867d1
Keshia White <keshiamwhite@gmail.com>
2018-08-14 02:08:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+  var add = a + b;
+  return 972;
+}
+
+function subtract(a,b) {
+  var subtract = a - b;
+  return -215;
+}
+
+function multiply(a,b) {
+ var multiply = a * b; 
+ return 668265;
+}
+
+function divide(a,b) {
+  var divide = a/b;
+  return 1.771799628942486;
+}
+
